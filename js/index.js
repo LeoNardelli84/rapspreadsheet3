cb838993ba3b8d38e897b87c846e6a5b4baf1ae4
LeoNardelli84 <siblings.distribuidora@gmail.com>
2023-05-04 16:47:22 -0300

    Primer commit, index casi terminado

---
+++ js/index.js
@@ -0,0 +1,57 @@
+//https://mpago.la/1g2F5th link de donaciones mercadopago
+//https://www.youtube.com/watch?v=OrMpKvcLPw8 conectar github con cpanel repo publico
+//https://9elements.github.io/fancy-border-radius/#52.85.16.51--. div deformados
+
+//------------
+function obtenerFecha(){
+
+    var fecha = new Date();
+    var dia = fecha.getDate();
+    var mes =  fecha.getMonth();
+    var anio = fecha.getFullYear();
+    var num_dia = fecha.getDay();
+    var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio","Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    var dias = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+
+    return ( dias[num_dia] + " " + dia + " de " + meses[mes] + " de " + anio);
+
+  }
+  var fecha = obtenerFecha();
+  $("#fecha").html(fecha);
+//------------
+
+  $(".formato").click(function(){
+    var formato = $(this).children('h4').html()
+    
+    Swal.fire({
+        title: formato,
+        text: "Comenzando...",
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonColor: 'black',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Comenzar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          
+          $(location).attr('href', './pages/regular.html')
+        }
+      })
+
+  });//FIN .FORMATO CLICK
+//------------
+
+//windows scroll
+$(window).scroll(function (event) {
+  var scroll = $(window).scrollTop();
+  console.log(scroll);
+
+  $(".presentacion").removeClass("invisible");
+  
+  $(".presentacion").addClass("animate__zoomIn");
+  $(".app").removeClass("invisible");
+  $(".app").addClass("animate__fadeInLeftBig");
+  $(".img-presentacion").addClass("animate__backInDown");
+ 
+  
+});
---
+++ readme.txt
@@ -0,0 +1,17 @@
+participantes de cada liga
+España: Sawi Elekipo y Mr. Ego
+Argentina: Dtoke, G Sony, CTZ, Replik, Cacha, Naista, Larrix, MP, Stuart, Klan, Dybbuk y Jaff
+Chile: Kaiser, Nitro, Basek, Joqerr, Acertijo, Teorema, Metalingúistica, Rodamiento, Racso y Satim
+México: Lobo López (Franco Escamilla), Lobo Estepario, Skiper, Rapder, Jony Beltrán, RC, Dante, Zticma, Majestic y Ari Carrillo
+Perú: Stick, Black Code, Jota, Kian, Vijay Kesh, Skill, Jair Wong, Diego Mc, Litzen y Piero Pistas
+Colombia: Marithea, Lokillo, Valles-T, Chang, Filósofo, Airon, RBN, Letra, Carpediem, Puppy, Ken Zingle y Coloso
+Caribe: Éxodo Lirical, SNK, Switch, Reverse, Aldahir, Yartzi, Ritmodelia y Zaki
+
+“LA REGÉNESIS”
+con tres formatos diferentes para la liga regular, los play-offs y las finales.
+El formato principal, que se utilizará durante gran parte de la temporada, constará de cuatro rondas: Incremental Mode, Random Mode, Minutos Libres y Deluxe.
+
+video explicativo
+https://www.youtube.com/watch?v=PXYO_-UX39c
+https://www.youtube.com/watch?v=G-RAFD4hqvg
+https://www.youtube.com/watch?v=oR7gLmEq2j0
